cea0f79218ae1a4ee0b8184933fdaa6034257592
Tischnee <wagnerfrosch@gmail.com>
2018-03-08 21:29:26 +0100

    Neue Funktionen
    - Zweites Plus zum Hinzufügen eines neuen Termins
    - Einfügen der Page in den Frame
    - auskommentierte Funktion kann vielleicht gelöscht werden ( jetzt jedoch noch nicht)

--- Test/Pages/Home.xaml.cs
+++ Test/Pages/Home.xaml.cs
@@ -29,7 +29,7 @@ namespace Test.Pages
         private ListDictionary Schüler;
         private ClientClassLib.Client client;
         private int K_ID;
-        private string[] namensspeicher = new string[5]; 
+        
         
         public Home()
         {
@@ -114,101 +114,95 @@ namespace Test.Pages
 
         private void terminHinzufügen_Click(object sender, RoutedEventArgs e)
         {
-            //stack_informationsausgabe.Visibility = Visibility.Collapsed;
-            
+            frame_informationsausgabe.Navigate(new ExtraPage.TerminErstellen());
+
             // Hinzufügen eines neuen Termins
             // Einfügen einer Page an die unten rechte Stelle !!!!!
-            //datenEingabefeldErstellen();
-            //ereignisseErstellen();      // Hinzufügen des neuen Termins auf die Pinnwand
+
 
             //Aktualisierung der Datenbank mit dem neuen Termin
         }
 
-        private void datenEingabefeldErstellen()
-        {
-            stack_informationsausgabe.Visibility = Visibility.Collapsed;
-            StackPanel stack_main = new StackPanel { Width = 390, Margin = new Thickness(10, 0, 0, 0) , Name ="stack_main" };
-            Label lbl_Überschrift = new Label { FontSize = 23, FontWeight = FontWeights.Bold, Margin = new Thickness(0, 5, 0, 10), Content = "Neuen Termin erstellen:" };
+        #region Ist nur noch wichtig wenn das mit dem Frame nicht gehen sollte
+        //private void datenEingabefeldErstellen()
+        //{
+        //    //stack_informationsausgabe.Visibility = Visibility.Collapsed;
+        //    StackPanel stack_main = new StackPanel { Width = 390, Margin = new Thickness(10, 0, 0, 0) , Name ="stack_main" };
+        //    Label lbl_Überschrift = new Label { FontSize = 23, FontWeight = FontWeights.Bold, Margin = new Thickness(0, 5, 0, 10), Content = "Neuen Termin erstellen:" };
 
-            #region Termin Namenseingabe
-            // Eingabe des Namens des Termins
-            StackPanel stack_Name = new StackPanel { Orientation = Orientation.Horizontal };
-            Label lbl_schrift1 = new Label { Content = "Terminname", Width = 80 };
-            TextBox txt_terminName = new TextBox { Width = 250, Height = 20, Margin = new Thickness(5, 0, 0, 0) , Name = "txt_terminName" };
+        //    #region Termin Namenseingabe
+        //    // Eingabe des Namens des Termins
+        //    StackPanel stack_Name = new StackPanel { Orientation = Orientation.Horizontal };
+        //    Label lbl_schrift1 = new Label { Content = "Terminname", Width = 80 };
+        //    TextBox txt_terminName = new TextBox { Width = 250, Height = 20, Margin = new Thickness(5, 0, 0, 0) , Name = "txt_terminName" };
 
-            stack_Name.Children.Add(lbl_schrift1);
-            stack_Name.Children.Add(txt_terminName);
-            #endregion
+        //    stack_Name.Children.Add(lbl_schrift1);
+        //    stack_Name.Children.Add(txt_terminName);
+        //    #endregion
 
-            #region Termin Datumseingabe
-            // Eingabe des Datums des Termins
-            StackPanel stack_Datum = new StackPanel { Orientation = Orientation.Horizontal, Margin = new Thickness(0, 10, 0, 0) };
-            Label lbl_schrift2 = new Label { Content = "Datum", Width = 80 };
-            //Combobox Tag
-            ComboBox cb_terminDatumTag = new ComboBox { Width = 40, Height = 25, Margin = new Thickness(5, 0, 0, 0) , Name = "cb_terminDatumTag"};
-            for (int i = 0; i < 31; i++)
-            {
-                cb_terminDatumTag.Items.Add(i + 1);
-            }
-            cb_terminDatumTag.SelectedIndex = 0;
-            //Combobox Monat
-            ComboBox cb_terminDatumMonat = new ComboBox { Width = 80, Height = 25, Margin = new Thickness(5, 0, 0, 0), Name = "cb_terminDatumMonat" };
-            CultureInfo ci = new CultureInfo("de-DE");
-            for (int i = 0; i < 12; i++)
-            {
-                cb_terminDatumMonat.Items.Add(ci.DateTimeFormat.MonthNames[i]);
-            }
-            cb_terminDatumMonat.SelectedIndex = 0;
-            //Combobox Jahr
-            ComboBox cb_terminDatumJahr = new ComboBox { Width = 80, Height = 25, Margin = new Thickness(5, 0, 0, 0), Name = "cb_terminDatumJahr" };
-            for (int i = DateTime.Now.Year; i < DateTime.Now.Year + 60; i++)
-            {
-                cb_terminDatumJahr.Items.Add(i);
-            }
-            cb_terminDatumJahr.SelectedIndex = 0;
-            //Hinzufügen zum Datums stack
-            stack_Datum.Children.Add(lbl_schrift2);
-            stack_Datum.Children.Add(cb_terminDatumTag);
-            stack_Datum.Children.Add(cb_terminDatumMonat);
-            stack_Datum.Children.Add(cb_terminDatumJahr);
-            #endregion
+        //    #region Termin Datumseingabe
+        //    // Eingabe des Datums des Termins
+        //    StackPanel stack_Datum = new StackPanel { Orientation = Orientation.Horizontal, Margin = new Thickness(0, 10, 0, 0) };
+        //    Label lbl_schrift2 = new Label { Content = "Datum", Width = 80 };
+        //    //Combobox Tag
+        //    ComboBox cb_terminDatumTag = new ComboBox { Width = 40, Height = 25, Margin = new Thickness(5, 0, 0, 0) , Name = "cb_terminDatumTag"};
+        //    for (int i = 0; i < 31; i++)
+        //    {
+        //        cb_terminDatumTag.Items.Add(i + 1);
+        //    }
+        //    cb_terminDatumTag.SelectedIndex = 0;
+        //    //Combobox Monat
+        //    ComboBox cb_terminDatumMonat = new ComboBox { Width = 80, Height = 25, Margin = new Thickness(5, 0, 0, 0), Name = "cb_terminDatumMonat" };
+        //    CultureInfo ci = new CultureInfo("de-DE");
+        //    for (int i = 0; i < 12; i++)
+        //    {
+        //        cb_terminDatumMonat.Items.Add(ci.DateTimeFormat.MonthNames[i]);
+        //    }
+        //    cb_terminDatumMonat.SelectedIndex = 0;
+        //    //Combobox Jahr
+        //    ComboBox cb_terminDatumJahr = new ComboBox { Width = 80, Height = 25, Margin = new Thickness(5, 0, 0, 0), Name = "cb_terminDatumJahr" };
+        //    for (int i = DateTime.Now.Year; i < DateTime.Now.Year + 60; i++)
+        //    {
+        //        cb_terminDatumJahr.Items.Add(i);
+        //    }
+        //    cb_terminDatumJahr.SelectedIndex = 0;
+        //    //Hinzufügen zum Datums stack
+        //    stack_Datum.Children.Add(lbl_schrift2);
+        //    stack_Datum.Children.Add(cb_terminDatumTag);
+        //    stack_Datum.Children.Add(cb_terminDatumMonat);
+        //    stack_Datum.Children.Add(cb_terminDatumJahr);
+        //    #endregion
 
-            #region Terminbeschreibung 
-            //Eingabe der Beschreibung des Termins
-            StackPanel stack_terminBeschreibung = new StackPanel { Orientation = Orientation.Horizontal, Margin = new Thickness(0, 10, 0, 0) };
-            Label lbl_schrift3 = new Label { Content = "Beschreibung", Width = 80 };
-            TextBox txt_TerminBeschreibung = new TextBox { Width = 250, Height = 145, Margin = new Thickness(5, 0, 0, 0) };
+        //    #region Terminbeschreibung 
+        //    //Eingabe der Beschreibung des Termins
+        //    StackPanel stack_terminBeschreibung = new StackPanel { Orientation = Orientation.Horizontal, Margin = new Thickness(0, 10, 0, 0) };
+        //    Label lbl_schrift3 = new Label { Content = "Beschreibung", Width = 80 };
+        //    TextBox txt_TerminBeschreibung = new TextBox { Width = 250, Height = 145, Margin = new Thickness(5, 0, 0, 0) };
 
-            stack_terminBeschreibung.Children.Add(lbl_schrift3);
-            stack_terminBeschreibung.Children.Add(txt_TerminBeschreibung);
-            #endregion
+        //    stack_terminBeschreibung.Children.Add(lbl_schrift3);
+        //    stack_terminBeschreibung.Children.Add(txt_TerminBeschreibung);
+        //    #endregion
 
-            Button cmd_hinzufügen = new Button { Width = 100 , Height = 35  ,Content = "Abschicken" ,Margin = new Thickness(180,10,0,0)};
-            cmd_hinzufügen.Click += new RoutedEventHandler(cmd_hinzufügen_Click);
+        //    Button cmd_hinzufügen = new Button { Width = 100 , Height = 35  ,Content = "Abschicken" ,Margin = new Thickness(180,10,0,0)};
+        //    cmd_hinzufügen.Click += new RoutedEventHandler(cmd_hinzufügen_Click);
 
-            // Namen im Array speichern
-            namensspeicher[0] = txt_terminName.Name;
-            namensspeicher[1] = cb_terminDatumTag.Name;
-            namensspeicher[2] = cb_terminDatumMonat.Name;
-            namensspeicher[3] = cb_terminDatumJahr.Name;
+        //    // Namen im Array speichern
+        //    namensspeicher[0] = txt_terminName.Name;
+        //    namensspeicher[1] = cb_terminDatumTag.Name;
+        //    namensspeicher[2] = cb_terminDatumMonat.Name;
+        //    namensspeicher[3] = cb_terminDatumJahr.Name;
 
 
-            //Hinzufügen der verschiedenen stacks zum Main stack
-            stack_chatUndInfoBereich.Children.Add(stack_main);
-            stack_main.Children.Add(lbl_Überschrift);
-            stack_main.Children.Add(stack_Name);
-            stack_main.Children.Add(stack_Datum);
-            stack_main.Children.Add(stack_terminBeschreibung);
-            stack_main.Children.Add(cmd_hinzufügen);
-        }
+        //    //Hinzufügen der verschiedenen stacks zum Main stack
+        //    stack_chatUndInfoBereich.Children.Add(stack_main);
+        //    stack_main.Children.Add(lbl_Überschrift);
+        //    stack_main.Children.Add(stack_Name);
+        //    stack_main.Children.Add(stack_Datum);
+        //    stack_main.Children.Add(stack_terminBeschreibung);
+        //    stack_main.Children.Add(cmd_hinzufügen);
+        //}
+        #endregion
 
-        private void cmd_hinzufügen_Click(object sender, EventArgs e)
-        {
-            Pages.Home home = UIHelper.FindVisualParent<Pages.Home>((Button)sender);
-            
-            
-            //stack_chatUndInfoBereich.FindName(namensspeicher[1]);
-        }
 
         #region Erstellen eines Ereignisses auf der eigenen Pinnwand
         private void ereignisseErstellen()
@@ -220,7 +214,10 @@ namespace Test.Pages
             gradientBrush.GradientStops.Add(new GradientStop(Color.FromArgb(255, 178, 178, 178), 0));
             gradientBrush.GradientStops.Add(new GradientStop(Color.FromArgb(255, 38, 139, 185), 1));
 
-            StackPanel stack_ereignis = new StackPanel { Width = 150, Height = 70, Background = gradientBrush };
+            Button cmd_ereignis = new Button { Width = 150, Height = 70, Background = gradientBrush, Content="Hallo" };
+            cmd_ereignis.Click += new RoutedEventHandler(aktiverButton);
+
+            StackPanel stack_ereignis = new StackPanel();
 
             Label lbl_namenEreignis = new Label { HorizontalContentAlignment = HorizontalAlignment.Center, FontSize = 17, Margin = new Thickness(0, 10, 0, 5), Foreground = Brushes.Black };
             lbl_namenEreignis.Content = "Hallo";    // Name des neuen Ereignisses
@@ -229,7 +226,8 @@ namespace Test.Pages
             lbl_datumEreignis.Content = "02.10.1998";   // Datum des neuen Ereignisses
 
             // Anzeigen auf der Pinnwand
-            ug_terminÜbersicht.Children.Add(stack_ereignis);
+            ug_terminÜbersicht.Children.Add(cmd_ereignis);
+            
             stack_ereignis.Children.Add(lbl_namenEreignis);
             stack_ereignis.Children.Add(lbl_datumEreignis);
 
@@ -240,5 +238,19 @@ namespace Test.Pages
         {
             // Löschen eines Termins
         }
+
+        // Wird von jedem Button aufgerufen wenn er geklickt wird --> Übergabe des Names
+        private void aktiverButton(object sender, RoutedEventArgs e)
+        {
+            // Aufrufen der Information über das Ereigniss identifizierne aus der DB mittels des einer ID?????
+            Button aktiverButton = (Button)sender;
+            aktiverButton.Background = Brushes.Black;
+        }
+
+        private void buttonHinzufügen(object sender, RoutedEventArgs e)
+        {
+            ereignisseErstellen();
+        }
+
     }
 }
